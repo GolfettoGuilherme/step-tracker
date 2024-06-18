d6f209f54832359a99a252df50cb1305eb63499f
Guilherme Golfetto <guilherme.golfetto@hotmail.com>
2024-06-17 23:04:00 -0300

    projeto xcode criado

---
+++ Step Tracker/Step_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Step_TrackerApp.swift
+//  Step Tracker
+//
+//  Created by Guilherme Golfetto on 17/06/24.
+//
+
+import SwiftUI
+
+@main
+struct Step_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
